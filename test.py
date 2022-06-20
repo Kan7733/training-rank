17eaee79384169bc1a75423f8d8d76701af9626c
Kan7733 <kan19940703@gmail.com>
2022-06-20 18:06:59 +0900

    引数指定

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import tweepy
 import pandas as pd
 import os
+import sys
 import psycopg2
 from datetime import datetime,timedelta
 
@@ -10,8 +11,28 @@ DB_NAME = os.environ['DB_NAME']
 DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['DB_PASS']
 BAERER = os.environ['BEARER_TOKEN']
-query = 'デッドリフト lang:ja -is:retweet'
-limit = 5000
+args = sys.argv
+if args[1] == "ベンチプレス":
+    tbl = "tbl_benchpress"
+elif args[1] == "スクワット":
+    tbl = "tbl_squat"
+elif args[1] == "デッドリフト":
+    tbl = "tbl_deadlift"
+elif args[1] == "bcaa":
+    tbl = "tbl_bcaa"
+elif args[1] == "eaa":
+    tbl = "tbl_eaa"
+elif args[1] == "クレアチン":
+    tbl = "tbl_creatine"
+else:
+    print(args[1])
+    print("augument error")
+    exit()
+print(args[1])
+print(tbl)
+
+query = '{subject} lang:ja -"コード" -"クーポン" -"楽天" -"Amazon" -is:retweet'.format(subject=args[1])
+limit = 30000
 
 def get_connection():
     return psycopg2.connect('postgresql://{user}:{password}@{host}:{port}/{dbname}'
@@ -22,7 +43,8 @@ def get_connection():
 now = datetime.now() #- timedelta(days=5) #開始日を指定
 now = now.replace(minute=0, second=0, microsecond=0)
 end_time_tweepy = str(now.isoformat())+'+09:00'
-start_time = now - timedelta(days=1) #終了日を指定
+start_time = now - timedelta(days=6) #終了日を指定
+print(now,start_time)
 start_time_tweepy = str(start_time.isoformat())+'+09:00'
 print(end_time_tweepy,start_time_tweepy)
 client = tweepy.Client(BAERER)
@@ -37,7 +59,7 @@ for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, start_ti
     try:
         conn = get_connection()
         cur = conn.cursor()
-        cur.execute(f"insert into tbl_deadlift (tweet_id, author_id,created_at, contents) values ({tweet.data['id']},'{tweet.data['author_id']}','{tweet.data['created_at']}','{tweet.data['text']}')")
+        cur.execute(f"insert into {tbl} (tweet_id, author_id,created_at, contents) values ({tweet.data['id']},'{tweet.data['author_id']}','{tweet.data['created_at']}','{tweet.data['text']}')")
         conn.commit()
         cur.close()
         conn.close()
